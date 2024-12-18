f656f8351f7f6af934b6a255d37b4613130efbf6
unknown <dducthanh04@gmail.com>
2024-12-18 18:32:26 +0700

    update image

--- src/App.js
+++ src/App.js
@@ -9,6 +9,317 @@ import image3 from "../src/img/3.jpg"
 import image4 from "../src/img/4.jpg"
 
 function App() {
+  const data = [
+    {
+    "url": "raw.githubusercontent.com",
+    "id": 1,
+    "content": "Đây là hôm tớ tỏ tình và được em đồng ý làm người yêu tớ 😊. Tớ vuii lắm 😊",
+      "images": [
+        {
+          "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/buoi1-1.jpg",
+          "id": "1"
+        },
+        {
+          "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/buoi1-2.jpg",
+          "id": "2"
+        },
+        {
+          "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/buoi1-3.jpg",
+          "id": "3"
+        },
+        {
+          "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/buoi1-4.jpg",
+          "id": "4"
+        }
+      ]
+    },
+    {
+      "url": "raw.githubusercontent.com",
+      "id": 2,
+      "content": "Chúng tớ đi xem phim nè",
+        "images": [
+          {
+            "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img1.jpg",
+            "id": "1"
+          },
+          {
+            "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img2.jpg",
+            "id": "2"
+          },
+          {
+            "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img3.jpg",
+            "id": "3"
+          },
+          {
+            "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img4.jpg",
+            "id": "4"
+          }
+        ]
+      },
+      {
+        "url": "raw.githubusercontent.com",
+        "id": 3,
+        "content": "Được em tổ chức sinh nhật cho tớ 🎂🙆‍♂️",
+          "images": [
+            {
+              "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img18.jpg",
+              "id": "1"
+            },
+          ]
+        },
+        {
+          "url": "raw.githubusercontent.com",
+          "id": 4,
+          "content": "Lần đầu trong đời tớ được đi chụp photoboot, đặc biệt hơn là cùng em",
+            "images": [
+              {
+                "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img25.jpg",
+                "id": "1"
+              },
+              {
+                "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img30.jpg",
+                "id": "2"
+              },
+              {
+                "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img27.jpg",
+                "id": "3"
+              },
+              {
+                "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img28.jpg",
+                "id": "4"
+              }, 
+              {
+                "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img29.jpg",
+                "id": "5"
+              },
+              {
+                "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img37.jpg",
+                "id": "6"
+              },
+            ]
+          },
+          {
+            "url": "raw.githubusercontent.com",
+            "id": 5,
+            "content": "Chúng tớ có nhiều kỉ niệm lắm đây nè",
+              "images": [
+                {
+                  "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img5.jpg",
+                  "id": "1"
+                },
+                {
+                  "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img6.jpg",
+                  "id": "2"
+                },
+                {
+                  "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img7.jpg",
+                  "id": "3"
+                },
+                {
+                  "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img8.jpg",
+                  "id": "4"
+                }, 
+                {
+                  "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img9.jpg",
+                  "id": "5"
+                },
+                {
+                  "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img10.jpg",
+                  "id": "6"
+                },
+                {
+                  "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img11.jpg",
+                  "id": "7"
+                },
+                {
+                  "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img12.jpg",
+                  "id": "8"
+                },  {
+                  "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img13.jpg",
+                  "id": "9"
+                },  {
+                  "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img14.jpg",
+                  "id": "10"
+                },  {
+                  "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img15.jpg",
+                  "id": "11"
+                },  {
+                  "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img16.jpg",
+                  "id": "12"
+                },  {
+                  "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img17.jpg",
+                  "id": "13"
+                },  {
+                  "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img19.jpg",
+                  "id": "14"
+                },  {
+                  "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img20.jpg",
+                  "id": "15"
+                },  {
+                  "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img21.jpg",
+                  "id": "16"
+                },  {
+                  "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img22.jpg",
+                  "id": "17"
+                },  {
+                  "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img23.jpg",
+                  "id": "18"
+                },  {
+                  "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img24.jpg",
+                  "id": "19"
+                },  {
+                  "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img10.jpg",
+                  "id": "20"
+                },  {
+                  "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img31.jpg",
+                  "id": "21"
+                },  {
+                  "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img32.jpg",
+                  "id": "22"
+                },  {
+                  "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img33.jpg",
+                  "id": "23"
+                },  {
+                  "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img34.jpg",
+                  "id": "24"
+                },  {
+                  "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img35.jpg",
+                  "id": "25"
+                },
+                {
+                  "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img38.jpg",
+                  "id": "26"
+                },
+                {
+                  "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img39.jpg",
+                  "id": "27"
+                },
+                {
+                  "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img40.jpg",
+                  "id": "28"
+                },
+                {
+                  "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img41.jpg",
+                  "id": "29"
+                },
+                {
+                  "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img42.jpg",
+                  "id": "30"
+                },
+                {
+                  "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img43.jpg",
+                  "id": "31"
+                },
+                {
+                  "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img44.jpg",
+                  "id": "32"
+                },
+                {
+                  "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img45.jpg",
+                  "id": "33"
+                },
+                {
+                  "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img46.jpg",
+                  "id": "34"
+                },
+                {
+                  "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img47.jpg",
+                  "id": "35"
+                },
+                {
+                  "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img48.jpg",
+                  "id": "36"
+                },
+                {
+                  "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img49.jpg",
+                  "id": "37"
+                },
+                {
+                  "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img50.jpg",
+                  "id": "38"
+                },
+                {
+                  "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img51.jpg",
+                  "id": "39"
+                },
+                {
+                  "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img52.jpg",
+                  "id": "40"
+                },
+                {
+                  "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img53.jpg",
+                  "id": "41"
+                },
+          
+
+                //hi
+                {
+                  "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img54.jpg",
+                  "id": "43"
+                },  {
+                  "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img55.jpg",
+                  "id": "44"
+                },  {
+                  "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img76.jpg",
+                  "id": "45"
+                },  {
+                  "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img57.jpg",
+                  "id": "46"
+                },  {
+                  "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img58.jpg",
+                  "id": "47"
+                },  {
+                  "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img59.jpg",
+                  "id": "48"
+                },  {
+                  "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img60.jpg",
+                  "id": "49"
+                },  {
+                  "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img77.jpg",
+                  "id": "50"
+                },  {
+                  "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img78.jpg",
+                  "id": "51"
+                },  {
+                  "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img79.jpg",
+                  "id": "52"
+                },  {
+                  "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img80.jpg",
+                  "id": "53"
+                },  {
+                  "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img81.jpg",
+                  "id": "54"
+                },  {
+                  "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img82.jpg",
+                  "id": "55"
+                },  {
+                  "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img83.jpg",
+                  "id": "56"
+                },  {
+                  "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img68.jpg",
+                  "id": "57"
+                },  {
+                  "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img70.jpg",
+                  "id": "59"
+                },  {
+                  "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img84.jpg",
+                  "id": "60"
+                },  {
+                  "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img72.jpg",
+                  "id": "61"
+                },  {
+                  "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img73.jpg",
+                  "id": "62"
+                },  {
+                  "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img74.jpg",
+                  "id": "63"
+                },  {
+                  "url": "https://raw.githubusercontent.com/nguyenducthanh04/server-image/main/image/img75.jpg",
+                  "id": "64"
+                },
+              ]
+            },
+]
   return (
     <div className="App">
       <header className="App-header">
@@ -37,18 +348,20 @@ function App() {
           <div className='title-img-all'>
             <h2>Dưới đây là kỉ niệm của chúng tớ </h2>
           </div>
-          <div className='item'>
+          {data?.map((dataItem) => (
+        <div className='item' key={dataItem.id}>
             <div className='content'>
-              <p>Đây là hôm tớ tỏ tình và được em đồng ý làm người yêu tớ 😊. Tớ vuii lắm 😊</p>
-              <img src={image1}></img>
-              <img src={image2}></img>
-              <img src={image3}></img>
-              <img src={image4}></img>
+              <p>{dataItem.content}</p>
+              {dataItem?.images?.map((img) => (
+                <img key={img.id} src={img.url}></img>
+              ))}
             </div>
-          </div>
+        </div>
+          ))}
+         
         </div>
         <div className='xiang'>
-          <em>Tớ mong tớ và em sẽ luôn vui vẻ và hạnh phúc cho đến mãi về sau này 🥰</em>
+          <em>Anh mong em sẽ luôn đồng hành và ở bên anh mãi về sau bởi vì em biết đó anh rất trân trọng em cũng như mối quan hệ này của hai taaaa 🥰</em>
         </div>
       </div>
     </div>
